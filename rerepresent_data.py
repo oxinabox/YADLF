4f2ba04ad8d76c665219ac817724b65c2a993145
oxinabox <oxinabox@a0b86eba-9b95-4faf-b860-5d9d438e4fb8>
2014-07-09 08:17:20 +0000

    =ready go phase3
    git-svn-id: http://svn.ucc.asn.au:8080/oxinabox/Uni%20Notes/honours/prototypes/ipython/yadlf@2086 a0b86eba-9b95-4faf-b860-5d9d438e4fb8

--- rerepresent_data.py
+++ rerepresent_data.py
@@ -2,24 +2,25 @@ import numpy as np
 import functools
 from  datasets.generic_dataset import LabelledDataset
 import datasets.dataset_util as dutil
+import numpyutil as nutil
 
 def code_dataset(model, depth, dataset):
+    assert(len(dataset)>0)
     data = dataset.data['data']
-    lbls = dataset.data['lbl']
+    lbls = dataset.data['lbls']
     coded_datalist = zip(model.get_code(data,depth), lbls)
     #OPTIMIMISE: this could be better, makign a list then it is turned ino an array in thethe LavelledDataset        
 
     return LabelledDataset(coded_datalist)
 
 def input_and_code_dataset(model, depth,dataset):
+    
     data = dataset.data['data']
-    lbls = dataset.data['lbl']
+    lbls = dataset.data['lbls']
     codes = model.get_code(data,depth)
     enhanced_data = np.concatenate((data,codes), axis=1)
     data_list = zip(enhanced_data, lbls) 
-
     #OPTIMIMISE: this could be better, makign a list then it is turned ino an array in thethe LavelledDataset        
-
     return LabelledDataset(data_list)
        
 
