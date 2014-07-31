538ad5e56d53299ef59cde28e3064db25c43d0a4
oxinabox <oxinabox@a0b86eba-9b95-4faf-b860-5d9d438e4fb8>
2014-07-31 06:58:33 +0000

    =moddified errors
    git-svn-id: http://svn.ucc.asn.au:8080/oxinabox/Uni%20Notes/honours/prototypes/ipython/yadlf@2171 a0b86eba-9b95-4faf-b860-5d9d438e4fb8

--- errors.py
+++ errors.py
@@ -4,8 +4,8 @@ class TrainingFailedError(RuntimeError):
     """
     Raised indicicating the Training Failed, 
     and the model is in a invalid state to read output"""
-    def __init__(self, inner_error):
+    def __init__(self, msg, inner_error):
         self.inner_error = inner_error
         self.traceback = sys.exc_info()
-        RuntimeError.__init__(self, "Training Error: " + inner_error.message)
+        RuntimeError.__init__(self, msg + "\n Inner Error: " + inner_error.message)
 
